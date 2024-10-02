d06fc7e06eff28d790ba8bb1971e1213466bbb4a
dumbledor90 <97580746+dumbledor90@users.noreply.github.com>
2024-10-02 23:40:56 +0700

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6ps8j!crjgrxt34cqbqn7x&b3y%(fn
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['dumbledor90.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['lumiauctionswebapp-production.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://dumbledor90.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['lumiauctionswebapp-production.up.railway.app']
 
 # Application definition
 
