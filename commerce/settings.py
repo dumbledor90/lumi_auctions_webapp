849030ad3ceffccfe6b13df8e3bb9f533a6afb92
dumbledor90 <97580746+dumbledor90@users.noreply.github.com>
2024-10-03 14:46:31 +0700

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['lumiauctionswebapp-production.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['lumiauctionswebapp-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://lumiauctionswebapp-production.up.railway.app']
 
 # Application definition
 
